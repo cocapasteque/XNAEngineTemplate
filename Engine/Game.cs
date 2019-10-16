382fcc75f91a8a12502d44a14bc8c41846767b6d
Antoine Thiry <a.thiry@str8labs.com>
2019-10-16 23:59:08 +0200

    Added Game

---
+++ Engine/Game.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Engine
+{
+    public class Game : Microsoft.Xna.Framework.Game
+    {
+        private GraphicsDeviceManager GraphicsDeviceManager { get; set; }
+        private SpriteBatch SpriteBatch { get; set; }
+
+        public Game()
+        {
+            GraphicsDeviceManager = new GraphicsDeviceManager(this)
+            {
+                PreferMultiSampling = true
+            };
+           
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            SpriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
+            
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.LightBlue);
+
+            SpriteBatch.Begin();
+            SpriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Launcher/Launcher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Engine;
+
+namespace Launcher
+{
+    static class Launcher
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Starting Game.");
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
